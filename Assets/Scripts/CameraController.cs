100f1d1a1b1adf511b7a6f97c45e35268202d573
Guilherme G. M <guilherme.menaldo@outlook.com>
2015-06-20 22:53:15 -0300

    04. Moving the Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject Player;
+
+	private Vector3 Offset;
+
+	void Start()
+	{
+		Offset = transform.position - Player.transform.position;
+	}
+
+	// Depois de todas as updates
+	void LateUpdate () {
+		transform.position = Player.transform.position + Offset;
+	}
+}
