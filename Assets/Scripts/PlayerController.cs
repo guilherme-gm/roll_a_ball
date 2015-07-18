98e80a2f274882e4638d66b4f3e7fb2a056b08d0
Guilherme G. M <guilherme.menaldo@outlook.com>
2015-07-18 12:01:46 -0300

    Adiciona o sistema de Powers (não testado)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,20 @@ public class PlayerController : MonoBehaviour {
 	public float Speed;
 	public Text CountText;
 	public Text WinText;
+	public Text GoodScText;
+	public Text BadScText;
 
 	private Rigidbody RBody;
 	private int Count;
+	private Power[] PowersData;
 
 	void Start ()
 	{
 		RBody = GetComponent<Rigidbody> ();
+
+		// Inicializa a lista de powers
+		this.PowersData = new Power[(int)Powers.MAX];
+
 		Count = 0;
 		SetCountText ();
 		WinText.text = "";
@@ -31,13 +38,82 @@ public class PlayerController : MonoBehaviour {
 
 	void OnTriggerEnter (Collider other)
 	{
-		if (other.gameObject.CompareTag ("Pick Up")) {
-			other.gameObject.SetActive(false);
+		if (other.gameObject.CompareTag (Tags.PickUp)) { // Ativaçao para Pick Ups
+			other.gameObject.SetActive (false);
 			Count++;
-			SetCountText();
+			SetCountText ();
+		} else if (other.gameObject.CompareTag (Tags.Power)) { // Ativacao para Powers
+			PowerObject pObj = other.gameObject.GetComponent<PowerObject>();
+			this.PowerStart(pObj.PowerData);
+			other.gameObject.SetActive(false);
 		}
 	}
 
+	//
+
+	/// <summary>
+	/// Chamado para iniciar o efeito de um Power
+	/// </summary>
+	/// <param name="pObj">O poder ativado</param>
+	private void PowerStart(Power pObj)
+	{
+		// Se o efeito ja esta em execucao, encerra
+		if (this.PowersData[(int)pObj.Type] != null)
+			StopCoroutine(this.PowersData[(int)pObj.Type].Routine);
+		
+		// Atualiza a informaçao sobre o efeito
+		this.PowersData [(int)pObj.Type] = pObj;
+		
+		// Inicia um novo timer
+		this.PowersData[(int)pObj.Type].Routine =  StartCoroutine(PowerTimer(pObj.Type, pObj.Duration));
+		
+		// Mostra a mensagem do efeito
+		if (pObj.IsGood) {
+			GoodScText.text = pObj.Name;
+			StartCoroutine(TimedDisplay(GoodScText.gameObject, 1f));
+		} else {
+			BadScText.text = pObj.Name;
+			StartCoroutine(TimedDisplay(BadScText.gameObject, 1f));
+		}
+	}
+	
+	/// <summary>
+	/// Coroutine para controlar o
+	/// tempo de um Power
+	/// </summary>
+	/// <returns>The timer.</returns>
+	/// <param name="powerType">Tipo do Power</param>
+	/// <param name="duration">Duracao em segundos</param>
+	private IEnumerator PowerTimer(Powers powerType, float duration)
+	{
+		yield return new WaitForSeconds (duration);
+		PowerEnd (powerType);
+	}
+
+	/// <summary>
+	/// Finaliza o efeito de um Power
+	/// </summary>
+	/// <param name="type">Type.</param>
+	private void PowerEnd(Powers type)
+	{
+		this.PowersData [(int)type].Duration = 0;
+	}
+
+	/// <summary>
+	/// Activates a GameObject for duration time
+	/// and hides it again
+	/// </summary>
+	/// <returns>The timer.</returns>
+	/// <param name="display">Display.</param>
+	/// <param name="duration">Duration.</param>
+	private IEnumerator TimedDisplay(GameObject display, float duration)
+	{
+		display.SetActive (true);
+		yield return new WaitForSeconds (duration);
+		display.SetActive (false);
+	}
+
+	//
 	void SetCountText()
 	{
 		CountText.text = "Count: " + Count.ToString();
---
+++ Assets/Scripts/PowerObject.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Contem uma lista de todos
+/// os Powers Ups/Downs.
+/// O ultimo elemento deve ser
+/// MAX, para a criaçao de arrays
+/// com tamanho fixo.
+/// </summary>
+public enum Powers {
+	// Deve sempre ser o ultimo
+	MAX
+}
+
+/// <summary>
+/// Define os dados de um power
+/// </summary>
+public class Power
+{
+	// O tipo de power
+	public Powers Type { get; set; }
+	// O nome (para display)
+	public string Name { get; set; }
+	// Valores que auxiliares
+	public int Val1 { get; set; }
+	public int Val2 { get; set; }
+	// Duraçao
+	public float Duration { get; set; }
+	// Up (true) / Down (False)
+	public bool IsGood { get; set; }
+	// Referencia a Coroutine para timer
+	public Coroutine Routine { get; set; }
+
+	// Checa se o Power esta ativo ou nao
+	public bool IsActive()
+	{
+		if (this.Duration > 0)
+			return true;
+		else
+			return false;
+	}
+}
+
+/// <summary>
+/// Usado para armazenar informaçoes
+/// do PowerUp relacionado a um objeto
+/// </summary>
+public class PowerObject : MonoBehaviour
+{
+	public Power PowerData { get; set; }
+}
